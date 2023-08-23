497f9f741198221fc566c5757c5ad9b278332fb4
nahidpavelc <nahidpavelchowdhury777@gmail.com>
2023-08-24 00:25:05 +0600

    complate navbar

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className='section' id='services'>
+      services
+    </div>
+  )
 };
 
 export default Services;
